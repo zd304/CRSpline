a3254d49012ae3ce84840c3936d6c00ad08ce12f
zd304 <568892758@qq.com>
2020-04-11 19:05:48 +0800

    CRSpline

---
+++ Assets/Editor/CRSplinePathEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(CRSplinePath))]
+public class CRSplinePathEditor : Editor
+{
+    void OnEnable()
+    {
+        style.fontStyle = FontStyle.Bold;
+        style.normal.textColor = Color.white;
+    }
+
+    void OnSceneGUI()
+    {
+        CRSplinePath _target = target as CRSplinePath;
+        if (_target.enabled)
+        {
+            if (_target.nodes.Length > 0)
+            {
+                Undo.RecordObject(_target, "Adjust CRSpline Path");
+
+                Handles.Label(_target.nodes[0], "Begin", style);
+                if (_target.nodes.Length > 2)
+                {
+                    for (int i = 1; i < _target.nodes.Length - 1; ++i)
+                    {
+                        Handles.Label(_target.nodes[i], "Point(" + i + ")");
+                    }
+                }
+                Handles.Label(_target.nodes[_target.nodes.Length - 1], "End", style);
+
+                for (int i = 0; i < _target.nodes.Length; ++i)
+                {
+                    _target.nodes[i] = Handles.PositionHandle(_target.nodes[i], Quaternion.identity);
+                }
+            }
+        }
+
+    }
+
+    GUIStyle style = new GUIStyle();
+}
---
+++ Assets/Scripts/CRSplinePath.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CRSplinePath : MonoBehaviour
+{
+    void Awake()
+    {
+        if (mPathes.ContainsKey(gameObject.name))
+        {
+            Debug.LogError("CR样条线路径名重复：" + gameObject.name);
+            //Destroy(gameObject);
+            return;
+        }
+        mPathes.Add(gameObject.name, this);
+
+        spline = new CRSpline(nodes);
+    }
+
+    void OnDestroy()
+    {
+        mPathes.Remove(gameObject.name);
+    }
+
+    void DrawPath(Color color)
+    {
+        if (nodes.Length == 0)
+            return;
+
+        Vector3 pervPt = spline.Interp(0);
+
+        Gizmos.color = color;
+        int SmoothAmount = nodes.Length * 20;
+        for (int i = 1; i <= SmoothAmount; ++i)
+        {
+            float pm = (float)i / SmoothAmount;
+            Vector3 curPt = spline.Interp(pm);
+
+            Gizmos.DrawLine(curPt, pervPt);
+
+            pervPt = curPt;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (nodes.Length >= 2 && !Application.isPlaying)
+            spline.Reset(nodes);
+        DrawPath(Color.cyan);
+
+        Vector3 center = Vector3.zero;
+        for (int i = 0; i < nodes.Length; ++i)
+        {
+            Vector3 node = nodes[i];
+            center += node;
+        }
+        center /= nodes.Length;
+        transform.position = center;
+    }
+
+    public static CRSplinePath GetPathByName(string name)
+    {
+        CRSplinePath path;
+        if (!mPathes.TryGetValue(name, out path))
+            return null;
+        return path;
+    }
+
+    public Vector3[] nodes = new Vector3[] { Vector3.zero, Vector3.zero };
+
+    [System.NonSerialized]
+    public CRSpline spline = null;
+
+    static Dictionary<string, CRSplinePath> mPathes = new Dictionary<string, CRSplinePath>();
+}
